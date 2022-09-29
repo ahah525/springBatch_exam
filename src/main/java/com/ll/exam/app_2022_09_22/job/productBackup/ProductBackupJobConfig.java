a0af690de0095f2464004989cf69a3dd13b67a24
ahah525 <ahah525@naver.com>
2022-09-29 11:20:58 +0900

    feat : ProductBackupJob Item 처리자 방식으로 구현

--- src/main/java/com/ll/exam/app_2022_09_22/job/productBackup/ProductBackupJobConfig.java
+++ src/main/java/com/ll/exam/app_2022_09_22/job/productBackup/ProductBackupJobConfig.java
@@ -1,5 +1,9 @@
 package com.ll.exam.app_2022_09_22.job.productBackup;
 
+import com.ll.exam.app_2022_09_22.app.product.entity.Product;
+import com.ll.exam.app_2022_09_22.app.product.entity.ProductBackup;
+import com.ll.exam.app_2022_09_22.app.product.repository.ProductBackupRepository;
+import com.ll.exam.app_2022_09_22.app.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -8,11 +12,18 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,9 +32,13 @@ public class ProductBackupJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    private final ProductRepository productRepository;  // 읽기 대상
+    private final ProductBackupRepository productBackupRepository;  // 쓰기 대상
+
+
     @Bean
-    public Job ProductBackupJob(Step productBackupStep1, CommandLineRunner initData) throws Exception {
-        initData.run(); // 이것을 실행해야 DB 생성됨
+    public Job productBackupJob(Step productBackupStep1, CommandLineRunner initData) throws Exception {
+        initData.run();
 
         return jobBuilderFactory.get("productBackupJob")
                 .start(productBackupStep1)
@@ -32,19 +47,49 @@ public class ProductBackupJobConfig {
 
     @Bean
     @JobScope
-    public Step productBackupStep1(Tasklet productBackupStep1Tasklet) {
+    public Step productBackupStep1(
+            ItemReader productReader,
+            ItemProcessor productToProductBackupProcessor,
+            ItemWriter productBackupWriter
+    ) {
         return stepBuilderFactory.get("productBackupStep1")
-                .tasklet(productBackupStep1Tasklet)
+                .<Product, ProductBackup>chunk(100) // 100개씩 처리
+                .reader(productReader)
+                .processor(productToProductBackupProcessor)
+                .writer(productBackupWriter)
                 .build();
     }
 
-    @Bean
     @StepScope
-    public Tasklet productBackupStep1Tasklet() {
-        return (contribution, chunkContext) -> {
-            log.debug("productBackupStep1Tasklet 실행됨!");
+    @Bean
+    public RepositoryItemReader<Product> productReader() {
+        return new RepositoryItemReaderBuilder<Product>()
+                .name("productReader")
+                .repository(productRepository)
+                .methodName("findAll")
+                .pageSize(100)
+                .arguments(Arrays.asList())
+                .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+                .build();
+    }
 
-            return RepeatStatus.FINISHED;
-        };
+    @StepScope
+    @Bean
+    public ItemProcessor<Product, ProductBackup> productToProductBackupProcessor() {
+        return product -> new ProductBackup(product);
+    }
+
+    @StepScope
+    @Bean
+    public ItemWriter<ProductBackup> productBackupWriter() {
+        return items -> items.forEach(item -> {
+            ProductBackup oldProductBackup = productBackupRepository.findByProductId(item.getProduct().getId()).orElse(null);
+
+            if ( oldProductBackup != null ) {
+                productBackupRepository.delete(oldProductBackup);
+            }
+
+            productBackupRepository.save(item);
+        });
     }
 }
